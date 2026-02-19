762a1fb4d1cd92ec52723e5da1f31499b5f18dca
MELVIN SUAN <xalbaji@gmail.com>
2026-02-19 22:27:44 +0800

    paldo

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -6,15 +6,9 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getRoot(): string {
+    // You can return either plain text or HTML
+    return `<h1>You have logged out</h1>
+            <p><a href="/login.html">Click here to login again</a></p>`;
   }
-  // main controller or app controller
-@Get()
-getRoot() {
-  return `<h1>You have logged out</h1>
-          <p><a href="/login.html">Click here to login again</a></p>`;
 }
-
-
-};
